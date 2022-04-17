9f96ac904deda69fb00aa674abd19aa3acaa756f
Samuel Cipriano <samciprianoh@gmail.com>
2022-04-17 12:19:11 -0300

    pass props in component

--- chapter-1/01-github-explorer/src/components/RepositoryList.jsx
+++ chapter-1/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,32 +1,18 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "Repository Name",
+  description: "Repository Description",
+  url: "",
+};
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>Repository Name</strong>
-          <p>Repository Description</p>
-          <a href="">Acessar repositório</a>
-        </li>
-
-        <li>
-          <strong>Repository Name</strong>
-          <p>Repository Description</p>
-          <a href="">Acessar repositório</a>
-        </li>
-
-        <li>
-          <strong>Repository Name</strong>
-          <p>Repository Description</p>
-          <a href="">Acessar repositório</a>
-        </li>
-
-        <li>
-          <strong>Repository Name</strong>
-          <p>Repository Description</p>
-          <a href="">Acessar repositório</a>
-        </li>
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
